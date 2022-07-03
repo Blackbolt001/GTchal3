ee9ed9c79db8a4dd13e8f35455b214db84fa258f
JoeG <jgossett73@gmail.com>
2022-07-03 17:26:23 -0400

    pushagain

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,65 +2,75 @@
 
 // Add event listener to generate button
 
+var button = document.getElementById('gen')
+button.addEventListener('click',Generate)
 
 function Generate() {
  
   var length = prompt("How many Characters? Please enter a number between 8 and 128 ")
-  alert("Your password will be " + length +" characters");
-  if (length < 8 && is > 128) alert("password must have between 8 and 128 character")
-  else 
-  CntGenerate()
+  console.log(length);
+    if (length < 8 || length > 128){ alert("password must have between 8 and 128 character")
+ return
 }
- function CntGenerate() {
+else if (length > 8 || length <128) {
+  alert("Your password will be " + length +" characters")
+  CtnGenerate()
+}
+
+
+  function CtnGenerate() {
+  
   var UpperCase = confirm("Would you like to include Uppercase letters?")
-  if (confirm == true) {
-    alert("Password will include Uppercase");
-    sessionStorage.setItem(Password1,UpperCase);
+  if (confirm == true) 
+  {
+   alert("Password will include Uppercase")
+  }
+  else (confirm == false)
+  {
+    alert("Password will not include Uppercase")
+}
+  var LowerCase = confirm("Would you like to include Lowercase Letters?")
+  if (confirm == true) 
+ alert("Password will include LowerCase")
+  
+  else 
+  
+   alert("Password will not include LowerCase")
+  
+
+
+  var Numbers = confirm("Would you like to include Numbers?")
+
+  if (confirm == true) 
+  { 
+    alert("Password will include Numbers")
   }
   else 
-    alert("Password will not include Uppercase")
+  {
+    alert("Password will not include Numbers")
   }
 
-  var LowerCase = confirm("Would you like to include Lowercase Letters?");
-  if (confirm == true) {
-    alert("Password will include LowerCase");
-    sessionStorage.setItem(Password2,LowerCase);
-  }
-  else {
-    alert("Password will not include LowerCase");
-  }
-  var Numbers = confirm("Would you like to include Numbers?");
-
-  if (confirm == true) { 
-    alert("Password will include Numbers");
-    sessionStorage.setItem(Password3,Numbers);
-  }
-  else {
-    alert("Password will not include Numbers");
-  }
-var special = confirm("would you like to have Special Characters?");
-if (confirm == true) {
-  alert("Password will include special Characters");
+var special = confirm("would you like to have Special Characters?")
+if (confirm == true) 
+{
+  alert("Password will include special Characters")
 }
-else {
-  alert("Password will not include special Characters");
+else 
+{
+  alert("Password will not include special Characters")
+
 }
 
 var i = Number
 
-for (let i =8; i < 128; i++) {
+for (let i =8; i < 128; i++){
   console.log(i);
 }
-
 for ( let i = 0; i < 26; i ++) {
-var Letter = (i + 0).toString(26);
+var Letter = (i + 10).toString(36)
 console.log(Letter)
 }
 for ( let i =0; i < 26; i++) {
-  var UpLetter = (i+0).toString(26).toUpperCase();
-  
-  console.log(UpLetter)
-}
-var special = [/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/]
-console.log(special)[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+  var UpLetter = (i+10).toString(36).toUpperCase()
+  console.log(UpLetter)}}}
 
